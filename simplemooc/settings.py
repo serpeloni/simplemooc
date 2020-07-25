1d34e30624a16fd75fdf957f77d6a6097a531df8
Felipe Rosa Serpeloni <felipes@unicamp.br>
2020-07-25 18:59:28 -0300

    - heroku config

--- simplemooc/settings.py
+++ simplemooc/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
+import dj_database_url
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -50,6 +53,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = "simplemooc.urls"
@@ -113,11 +117,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = "/static/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "/media/"
 
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # E-mails
 # EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
@@ -136,3 +142,7 @@ LOGIN_URL = "accounts:login"
 LOGIN_REDIRECT_URL = "core:home"
 LOGOUT_URL = "accounts:logout"
 AUTH_USER_MODEL = "accounts.User"
+
+
+django_heroku.settings(locals())
+DATABASES["default"] = dj_database_url.config(conn_max_age=600, ssl_require=True)
