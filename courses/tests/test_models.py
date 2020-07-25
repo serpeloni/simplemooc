1f324c5e5a55ffe9a3ca7a8005622f529749f603
Felipe Rosa Serpeloni <felipes@unicamp.br>
2020-07-25 18:23:44 -0300

    - remove pycache - add tests

---
+++ courses/tests/test_models.py
@@ -0,0 +1,27 @@
+from courses.models import Course
+from django.test.client import Client
+from django.test import TestCase
+
+from model_bakery import baker
+
+
+class CourseManagerTestCase(TestCase):
+    def setUp(self):
+        self.courses_django = baker.make(
+            "courses.Course", name="Python na Web com Django", _quantity=5
+        )
+        self.courses_dev = baker.make(
+            "courses.Course", name="Python para Devs", _quantity=10
+        )
+        self.client = Client()
+
+    def tearDown(self):
+        Course.objects.all().delete()
+
+    def test_course_search(self):
+        search = Course.objects.search("django")
+        self.assertEqual(len(search), 5)
+        search = Course.objects.search("devs")
+        self.assertEqual(len(search), 10)
+        search = Course.objects.search("python")
+        self.assertEqual(len(search), 15)
