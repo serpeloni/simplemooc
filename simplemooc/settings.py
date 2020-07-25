5882efb86c0adbe46692a52ea0d91c276aed97a6
Felipe Rosa Serpeloni <felipes@unicamp.br>
2020-07-25 19:18:41 -0300

    static dirs

--- simplemooc/settings.py
+++ simplemooc/settings.py
@@ -122,7 +122,7 @@ STATIC_URL = "/static/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "/media/"
 
-# STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 # STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # E-mails
